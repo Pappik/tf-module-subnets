3acc0e6bb60d40b65846c53765eb76c7c3920dbd
pappik <pappik@gmail.com>
2024-01-02 22:22:14 -0500

    vpc terraform

--- main.tf
+++ main.tf
@@ -1,50 +1,37 @@
-resource "aws_subnet" "subnets" {
-  count  = length(var.cidr_block)
-  cidr_block = var.cidr_block[count.index]
-  availability_zone = var.availability_zone[count.index]
+module "subnets" {
+  source = "./subnets"
+  cidr_block = var.cidr_block
+  default_vpc_id = var.default_vpc_id
+  env = var.env
+  name = var.name
   vpc_id = var.vpc_id
-
-  tags = merge(local.common_tags, { Name = "${var.env}-${var.name}-subnet-${count.index+1}"} )
-
+  vpc_peering_connection_id = var.vpc_peering_connection_id
 }
 
-resource "aws_route_table" "route_table" {
-  vpc_id = var.vpc_id
 
-
-  route {
-    cidr_block        = data.aws_vpc.default.cidr_block
-    vpc_peering_connection_id = var.vpc_peering_connection_id
-  }
-
-  tags = merge(local.common_tags, { Name = "${var.env}-${var.name}-route_table" } )
-}
-
-resource "aws_route_table_association" "route-assoc" {
-  count          = length(aws_subnet.subnets)
-  subnet_id      = aws_subnet.subnets.*.id[count.index]
-  route_table_id = aws_route_table.route_table.id
-}
-
-resource "aws_route" "internet_gw_route" {
-  count       = var.internet_gw ? 1 : 0
-  route_table_id = ""
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(local.common_tags, { Name = "${var.env}-igw"} )
-
-}
-
-resource "aws_eip" "eip" {
-   domain   = "vpc"
-}
-
-resource "aws_nat_gateway" "ngw" {
-  allocation_id = aws_eip.eip.id
-  subnet_id     = aws_subnet.public.*.id[0]
-
-  tags = merge(local.common_tags, { Name = "${var.env}-natgw"} )
-}
+#resource "aws_route" "internet_gw_route" {
+#  count       = var.internet_gw ? 1 : 0
+#  route_table_id = aws_route_table.route_table.id
+#  destination_cidr_block = "0.0.0.0/0"
+#  gateway_id = var.internet_gw
+#}
+#
+#resource "aws_internet_gateway" "igw" {
+#  count       = var.internet_gw ? 1 : 0
+#  vpc_id = var.vpc_id
+#
+#  tags = merge(local.common_tags, { Name = "${var.env}-igw"} )
+#
+#}
+#
+#resource "aws_eip" "eip" {
+#   domain   = "vpc"
+#}
+#
+#resource "aws_nat_gateway" "ngw" {
+#  count         = var.nat_gw ? 1 : 0
+#  allocation_id = aws_eip.eip.id
+#  subnet_id     = aws_subnet.public.*.id[0]
+#
+#  tags = merge(local.common_tags, { Name = "${var.env}-ngw"} )
+#}
